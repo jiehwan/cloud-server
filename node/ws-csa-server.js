a9d9816af2ce2a57d932d0281bca527b1b982ac3
dockerfw <dockerfw@docserver.com>
2017-04-14 19:02:37 +0900

    simple connection is passed

--- node/ws-csa-server.js
+++ node/ws-csa-server.js
@@ -2,13 +2,15 @@
 
 "use strict";
 
+//////////////////////////////////////////////////////////////
+///     websocket server for csa interface
+//////////////////////////////////////////////////////////////
 const WebSocket = require("ws");
-
 const wss = new WebSocket.Server({ 
 	port: 4000 
 });
+console.log('csa port = ', wss.options.port);
 
-console.log('port = ', wss.options.port);
 
 function send(ws) {
         ws.send(JSON.stringify({ cmd: 'connected', 
@@ -21,21 +23,27 @@ function send(ws) {
 		});
 }
 
+var readline = require('readline');
+var rl = readline.createInterface({
+	input: process.stdin,
+        output: process.stdout,
+        terminal: false
+});
+
+var ws_csa;
+
 wss.on("connection", function connection(ws) {
         console.info("websocket connection open");
 
 	ws.on('message', function incoming(message) {
 		console.log('received: %s', message);
 		send(ws);
+		send_web(message)
 		});
 
         if (ws.readyState === ws.OPEN) {
-		var readline = require('readline');
-		var rl = readline.createInterface({
-			input: process.stdin,
-			output: process.stdout,
-			terminal: false
-		});
+		ws_csa = ws;
+ 
 		rl.on('line', function(line){
 			if (line.length > 0){
 				console.log("send :", JSON.stringify({ cmd: line }));
@@ -50,3 +58,56 @@ wss.on("connection", function connection(ws) {
 		});
 	}
 });
+
+
+
+//////////////////////////////////////////////////////////////
+///	websocket server for web interface
+//////////////////////////////////////////////////////////////
+const WebSocketIn = require("ws");
+const wssin = new WebSocketIn.Server({
+        port: 4002
+});
+console.log('internal port = ', wssin.options.port);
+
+var ws_web;
+
+function send_web(message) {
+	if(ws_web){
+
+	        //ws_web.send(JSON.stringify({ cmd: 'connected', token: 'test-token', clientnum: 888}),
+		ws_web.send(message,
+	                function (err) {
+        	                if (err) {
+                	                console.log("ws.send connected :", err);
+                        	}
+	                });
+		}
+}
+
+
+wssin.on("connection", function connection(ws) {
+        console.info("internal port connected...");
+	ws_web = ws;
+
+        ws.on('message', function incoming(message) {
+                console.log('internal port received: %s', message);
+
+		if( ws_csa){
+			console.log("send command =", message);
+			//ws_csa.send(JSON.stringify({ cmd: message }),
+			ws_csa.send(message,
+                                        function (err) {
+                                                if(err) {
+                                                        console.log("ws.send :", err);
+                                                }
+                                        });
+			}
+                });
+
+        if (ws.readyState === ws.OPEN) {
+
+                }
+
+});
+
--- node/ws-web-server.js
+++ node/ws-web-server.js
@@ -2,13 +2,15 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
 
+var port = 4001;
+
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
-server.listen(5000, function() {
-    console.log((new Date()) + ' Server is listening on port 5000');
+server.listen(port, function() {
+    console.log((new Date()) + ' Server is listening on port '+ port);
 });
 
 wsServer = new WebSocketServer({
@@ -26,6 +28,8 @@ function originIsAllowed(origin) {
   return true;
 }
 
+var connection;
+
 wsServer.on('request', function(request) {
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
@@ -34,12 +38,17 @@ wsServer.on('request', function(request) {
       return;
     }
 
-    var connection = request.accept("ws-web-server", request.origin);
+    connection = request.accept(null, request.origin);
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
+/////////////////////////////////////////////////////////////////////////////
+
             console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+
+	    wsc.send(JSON.stringify({cmd:message.utf8Data}));
+        
+/////////////////////////////////////////////////////////////////////////////
         }
         else if (message.type === 'binary') {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
@@ -50,3 +59,38 @@ wsServer.on('request', function(request) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+const WebSocket = require('ws');
+var wsc;
+var wscUri = 'ws://localhost:4002';
+
+function wsConnect() {
+	console.log("connect",wscUri);
+	
+	//try { ws = new WebSocket(wscUri); }
+	//catch (e) {console.log("WebSocket error :", e);} 
+
+	wsc = new WebSocket(wscUri);
+	
+
+       	wsc.onmessage = function(msg) {
+                var line = "";
+               	var data = msg.data;
+                line += "<p>"+data+"</p>";
+
+	        console.log("receive data from csa : " + line);
+
+		//connection.sendUTF("From ws-web-server: " + line.utf8Data);
+		connection.send("From ws-web-server: " + line);
+       	}
+        wsc.onopen = function() {
+       	        //ws.send("Open for data");
+               	console.log("connected");
+        }
+       	wsc.onclose = function() {
+               	console.log("closed");
+        }
+}
+
+wsConnect();
+
