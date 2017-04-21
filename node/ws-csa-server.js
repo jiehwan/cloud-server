160e3e17816178bbc90d5afb1ec1bec6d07d4d83
dockerfw <dockerfw@docserver.com>
2017-04-21 18:25:05 +0900

    added imageupdate feature

--- node/ws-csa-server.js
+++ node/ws-csa-server.js
@@ -2,20 +2,23 @@
 
 "use strict";
 
+var LinkedList = require('linkedlist');
+var ws_client_list = new LinkedList();  // connection-socket lists
+var TargetInfoList = new LinkedList();  // socket list received targetinfo data
+
 //////////////////////////////////////////////////////////////
 ///     websocket server for csa interface
 //////////////////////////////////////////////////////////////
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ 
-	port: 4000 
+const wss = new WebSocket.Server({
+	port: 4000
 });
 console.log('csa port = ', wss.options.port);
 
 
 function send_connection(ws) {
-
-		ws.send(JSON.stringify({ cmd: 'connected', 
-				token: ws.upgradeReq.headers['sec-websocket-key'], 
+		ws.send(JSON.stringify({ cmd: 'connected',
+				token: ws.upgradeReq.headers['sec-websocket-key'],
 				clientnum: ws._socket._server._connections}),
 		function (err) {
 			if (err) {
@@ -24,37 +27,45 @@ function send_connection(ws) {
 		});
 }
 
-function send_mesg_direct(message) {
+// send message into client
+//		message = format of CSA
+// need to add destination
+//		destination = "" : send to all connected clients
+//		destination = [ID] : ??????????????????????
+function send_mesg_direct(sendMsg, destClient) {
 	var index;
-	
+
 	// clear response data
 	TargetInfoList.resetCursor();
 	while (TargetInfoList.next()) {
 		TargetInfoList.removeCurrent();
 		TargetInfoList.resetCursor();
 	}
-	
-	list.resetCursor();
-    while (list.next()) {
-			console.log("-----------------------");		
-			console.log("list.length =", list.length)
-	        console.log("<direct> keyID ={%s} client{%s}", list.current.upgradeReq.headers['sec-websocket-key'], list.current.clientname);
-			console.log("<direct> send command =", message);
 
-			list.current.send(message, function (err) {
-				        	        if(err) {
-            					        console.info("<direct>ws.send :", err);
-										console.log("remove keyID ={%s} client{%s}", list.current.upgradeReq.headers['sec-websocket-key'], list.current.clientname);
-										list.current.close();
-										console.log("remove linst{%s} ", list.removeCurrent());
-										list.resetCursor();
-                					}
-				                });
-			index ++;
+	ws_client_list.resetCursor();
+	while (ws_client_list.next()) {
+		console.log("-----------------------");
+		console.log("list.length =", ws_client_list.length)
+		console.log("<direct> keyID ={%s} client{%s}", ws_client_list.current.upgradeReq.headers['sec-websocket-key'], ws_client_list.current.clientname);
+		console.log("<direct> send command: " + sendMsg);
+		console.log("<direct> destClient: " + destClient);
+
+		if((destClient == "ALL") || (destClient == ws_client_list.current.clientname)) {
+			console.log("<direct> sent to clientname(%s)", ws_client_list.current.clientname);
+			ws_client_list.current.send(sendMsg, function (err) {
+				if(err) {
+					// if error, remove the client in lists
+					console.info("<direct>ws.send :", err);
+					console.log("remove keyID ={%s} client{%s}", ws_client_list.current.upgradeReq.headers['sec-websocket-key'], ws_client_list.current.clientname);
+					ws_client_list.current.close();
+					console.log("remove linst{%s} ", ws_client_list.removeCurrent());
+					ws_client_list.resetCursor();
+				}
+			});
+		}
+		index ++;
     }
-
-	console.log("list.length =", list.length)
-
+	console.log("list.length =", ws_client_list.length)
 }
 
 
@@ -71,13 +82,7 @@ var rl = readline.createInterface({
 var connections = new Map();
 var connectionID;
 
-var LinkedList = require('linkedlist');
-var list = new LinkedList();
-
-
-var TargetInfoList = new LinkedList();
-
-function responseToWeb(ws, message) { 
+function responseToWeb(ws, message) {
 	console.log("rcv keyID ={%s} client{%s}", ws.upgradeReq.headers['sec-websocket-key'], ws.clientname);
 	console.log("message =", message);
 
@@ -85,6 +90,10 @@ function responseToWeb(ws, message) {
 	var rcv = JSON.parse(message);
 	console.log("<toWeb> message ={%s}", message);
 	console.log("<toWeb> cmd ={%s}", rcv.Cmd);
+
+	////////////////////////////////////
+	// switch on command from CSA
+	////////////////////////////////////
 	if(rcv.Cmd == 'GetContainersInfo')
 	{
 		console.log("<toWeb> TargetInfoList length =%d", TargetInfoList.length);
@@ -100,8 +109,8 @@ function responseToWeb(ws, message) {
 			}
 		}
 		if(found == 0) {
-				console.log("<toWeb> add new clientname ={%s}", list.current.clientname);
-				
+				console.log("<toWeb> add new clientname ={%s}", ws_client_list.current.clientname);
+
 				var targetInfoObject = { "key": "", "client": "",  "body": "" };
 				targetInfoObject.client = ws.clientname;
 				targetInfoObject.key = ws.upgradeReq.headers['sec-websocket-key'];
@@ -114,7 +123,8 @@ function responseToWeb(ws, message) {
 		if(TargetInfoList.length)
 		{
 			var msg_str = "";
-			
+
+			// construct data into array string
 			TargetInfoList.resetCursor();
 			while (TargetInfoList.next()) {
 				if(TargetInfoList.head == TargetInfoList.current) {
@@ -130,6 +140,7 @@ function responseToWeb(ws, message) {
 					msg_str += "]";
                 }
 			}
+			// send data to web...
 			send_web(msg_str);
 
 
@@ -151,7 +162,7 @@ function responseToWeb(ws, message) {
 }
 
 wss.on("connection", function connection(ws) {
-        console.info("websocket connection open");
+	console.info("websocket connection open");
 
 	// session management
 	////////////////////////////////////////////////////////////////////////
@@ -164,25 +175,23 @@ wss.on("connection", function connection(ws) {
 	//var sendThis = String(connectionID);
 	//var obj[connectionID] = JSON.parse(JSON.stringify(ws));
 
-	//list.push(connectionID);
 
 	ws.on('message', function incoming(message) {
 		console.log('received: %s', message);
 		console.log('received cmd: %s', JSON.parse(message).cmd);
 
 		var rcv = JSON.parse(message);
-		switch(rcv.cmd){
+		switch(rcv.cmd) {
 			case 'request':
 				ws.clientname = rcv.name;
 
-				list.push(ws);
-				console.log("list.length =", list.length)
+				ws_client_list.push(ws);
+				console.log("list.length =", ws_client_list.length)
 				console.log("ws.connections =", ws._socket._server._connections);
-				list.resetCursor();
-				while (list.next()) {
-					console.log("keyID ={%s} client ={%s}", list.current.upgradeReq.headers['sec-websocket-key'], list.current.clientname);
+				ws_client_list.resetCursor();
+				while (ws_client_list.next()) {
+					console.log("keyID ={%s} client ={%s}", ws_client_list.current.upgradeReq.headers['sec-websocket-key'], ws_client_list.current.clientname);
 				}
-
 				send_connection(ws);
 				break;
 			default:
@@ -192,38 +201,35 @@ wss.on("connection", function connection(ws) {
 		//responseToWeb(this, message);
 	});
 
-    if (ws.readyState === ws.OPEN) {
-
-		// input from prompt 
-		rl.on('line', function(line){
-			if (line.length > 0){
+	if (ws.readyState === ws.OPEN) {
+		// input from prompt
+		rl.on('line', function(line) {
+			if (line.length > 0) {
 				console.log("-----------------------");
 				console.log("<shell> [%s] send :", ws.clientname, JSON.stringify({ cmd: line }));
-				ws.send(JSON.stringify({ cmd: line }),
-					function (err) {
-						if(err) {
-							console.info("<shell>ws.send :", err);
+				ws.send(JSON.stringify({ cmd: line }), function (err) {
+					if(err) {
+						console.info("<shell>ws.send :", err);
 
-							// remove ws in list
-							list.resetCursor();
-							while (list.next()) {
-								if(list.current.upgradeReq.headers['sec-websocket-key'] == ws.upgradeReq.headers['sec-websocket-key']){
-									list.removeCurrent();
-									console.log("<shell> remove KeyID ={%s} client{%s}", list.current.upgradeReq.headers['sec-websocket-key'], list.current.clientname);
-									list.removeCurrent();
-									ws.close();
-									break;
-								}
+						// remove ws in list
+						ws_client_list.resetCursor();
+						while (ws_client_list.next()) {
+							if(ws_client_list.current.upgradeReq.headers['sec-websocket-key'] == ws.upgradeReq.headers['sec-websocket-key']) {
+								ws_client_list.removeCurrent();
+								console.log("<shell> remove KeyID ={%s} client{%s}", ws_client_list.current.upgradeReq.headers['sec-websocket-key'], ws_client_list.current.clientname);
+								ws_client_list.removeCurrent();
+								ws.close();
+								break;
 							}
-							ws.close();
-							return;
 						}
-					});
+						ws.close();
+						return;
+					}
+				});
 			}
 		});
 	}
 });
-
 
 
 //////////////////////////////////////////////////////////////
@@ -231,40 +237,69 @@ wss.on("connection", function connection(ws) {
 //////////////////////////////////////////////////////////////
 const WebSocketIn = require("ws");
 const wssin = new WebSocketIn.Server({
-        port: 4002
+	port: 4002
 });
 console.log('internal port = ', wssin.options.port);
 
-
 var ws_web;
-
 function send_web(message) {
 	if(ws_web){
-
-	        //ws_web.send(JSON.stringify({ cmd: 'connected', token: 'test-token', clientnum: 888}),
-		ws_web.send(message,
-	                function (err) {
-        	                if (err) {
-                	                console.log("ws.send connected :", err);
-                        	}
-	                });
-		}
+		//ws_web.send(JSON.stringify({ cmd: 'connected', token: 'test-token', clientnum: 888}),
+		ws_web.send(message, function (err) {
+			if (err) {
+				console.log("ws.send connected :", err);
+			}
+		});
+	}
 }
 
+function convertMsg_from_web_to_csa(message) {
+	var rcv = JSON.parse(message);
+	var sendMsg = "";
+	var destClient = "";
+
+	////////////////////////////////////
+	// switch on command from WEB
+	////////////////////////////////////
+	switch(rcv.Cmd) {
+		case "GetContainersInfo":
+			sendMsg = {
+				"Cmd": rcv.Cmd
+			}
+			destClient = "ALL";
+			break;
+		case "UpdateImage":
+			sendMsg = {
+				"Cmd": rcv.Cmd,
+				"ContainerName": rcv.ContainerName,
+				"ImageName": rcv.ImageName,
+			}
+			destClient = rcv.DeviceID;
+			break;
+		default:
+			break;
+	}
+	return{msg:sendMsg, dest:destClient}
+}
 
 wssin.on("connection", function connection(ws) {
-        console.info("internal port connected...");
+	console.info("internal port connected...");
+
 	ws_web = ws;
+	ws.on('message', function incoming(message) {
+		console.log('internal port received: %s', message);
 
-        ws.on('message', function incoming(message) {
-                console.log('internal port received: %s', message);
+		// conver message(WEB) into message(CSA)
+		// conver_mesg_from_web_to_csa();
+		var rcvVal = convertMsg_from_web_to_csa(message);
+		var sendMsg = JSON.stringify(rcvVal.msg);
+		var destClient = rcvVal.dest;
 
-				send_mesg_direct(message);
-                });
+		// add destination..
+		send_mesg_direct(sendMsg, destClient);
+	});
 
-        if (ws.readyState === ws.OPEN) {
+	if (ws.readyState === ws.OPEN) {
 
-                }
-
+	}
 });
-
--- node/ws-web-server.js
+++ node/ws-web-server.js
@@ -5,59 +5,57 @@ var http = require('http');
 var port = 4001;
 
 var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
+	console.log((new Date()) + ' Received request for ' + request.url);
+	response.writeHead(404);
+	response.end();
 });
 server.listen(port, function() {
-    console.log((new Date()) + ' Server is listening on port '+ port);
+	console.log((new Date()) + ' Server is listening on port '+ port);
 });
 
 wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
+	httpServer: server,
+	// You should not use autoAcceptConnections for production
+	// applications, as it defeats all standard cross-origin protection
+	// facilities built into the protocol and the browser.  You should
+	// *always* verify the connection's origin and decide whether or not
+	// to accept it.
+	autoAcceptConnections: false
 });
 
 function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
+	// put logic here to detect whether the specified origin is allowed.
+	return true;
 }
 
 var connection;
 
 wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
+	if (!originIsAllowed(request.origin)) {
+		// Make sure we only accept requests from an allowed origin
+		request.reject();
+		console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+		return;
+	}
 
-    connection = request.accept(null, request.origin);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-/////////////////////////////////////////////////////////////////////////////
+	connection = request.accept(null, request.origin);
+	console.log((new Date()) + ' Connection accepted.');
+	connection.on('message', function(message) {
+		if (message.type === 'utf8') {
+			/////////////////////////////////////////////////////////////////////////////
+			console.log('Received Message: ' + message.utf8Data);
+			wsc.send(message.utf8Data);
+			/////////////////////////////////////////////////////////////////////////////
+		}
+		else if (message.type === 'binary') {
+			console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+			connection.sendBytes(message.binaryData);
+		}
+	});
 
-            console.log('Received Message: ' + message.utf8Data);
-
-		    wsc.send(JSON.stringify({cmd:message.utf8Data}));
-        
-/////////////////////////////////////////////////////////////////////////////
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+	connection.on('close', function(reasonCode, description) {
+		console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+	});
 });
 
 const WebSocket = require('ws');
@@ -66,34 +64,32 @@ var wscUri = 'ws://localhost:4002';
 
 function wsConnect() {
 	console.log("connect :",wscUri);
-	
-	wsc = new WebSocket(wscUri);
 
-   	wsc.onmessage = function(msg) {
-	    console.log("receive data from csa : " + msg.data);
+	wsc = new WebSocket(wscUri);
+	wsc.onmessage = function(msg) {
+		console.log("receive data from csa : " + msg.data);
 		connection.send(msg.data);
-    }
+	}
 
 	wsc.onopen = function() {
-            //ws.send("Open for data");
-           	console.log("connected");
-    }
-    
+		//ws.send("Open for data");
+		console.log("connected");
+	}
+
 	wsc.onclose = function() {
-           	console.log("closed");
-			setTimeout( function() {
-				wsConnect();
-				}, 1000);
-    }
+		console.log("closed");
+		setTimeout( function() {
+			wsConnect();
+		}, 1000);
+	}
 }
 
 process.on('uncaughtException', function (err) {
 	console.log(err);
 	setTimeout(function(){
-			console.log("timeout~~");
-	        wsConnect();
-			},1000);
-}); 
+		console.log("timeout~~");
+		wsConnect();
+	},1000);
+});
 
 wsConnect();
-
